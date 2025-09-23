fb98c971f7be95b9ab40754db8e937c174bc636e
Stefan Schmerda <stefan.schmerda@gmail.com>
2025-09-23 09:13:30 +0200

    add vs code hotkey show function arguments

--- keyboards/holykeebs_custom/sweeq/keymaps/ss_via/keymap.c
+++ keyboards/holykeebs_custom/sweeq/keymaps/ss_via/keymap.c
@@ -34,9 +34,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,            KC_J,    KC_L,    KC_U,    KC_Y,    MO6,
-    HM_A,    HM_R,    HM_S,    HM_T,    KC_D,            KC_H,    HM_N,    HM_E,    HM_I,    HM_O,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_K,    KC_M,    KC_NO,   MO7,     MO5,
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,            KC_J,    KC_L,    KC_U,     KC_Y,    MO6,
+    HM_A,    HM_R,    HM_S,    HM_T,    KC_D,            KC_H,    HM_N,    HM_E,     HM_I,    HM_O,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_K,    KC_M,    KC_NO,    MO7,     MO5,
                                     MO3, LTSPC, MO1, MO3
   ),
 
@@ -79,19 +79,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LCTL(KC_7),    LCTL(KC_8),    LCTL(KC_9),    KC_NO,         KC_NO,                KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     LCTL(KC_4),    LCTL(KC_5),    LCTL(KC_6),    LCTL(KC_0),    HK_S_MODE,            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     LCTL(KC_1),    LCTL(KC_2),    LCTL(KC_3),    KC_NO,         HK_D_MODE,            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                                    KC_BTN1, KC_BTN2, KC_TRNS, MO8
+                                    KC_BTN1, KC_BTN2, KC_TRNS, KC_TRNS
   ),
 
   [7] = LAYOUT(
-    G(KC_DOT),    KC_NO,        KC_NO,         G(KC_P),      KC_NO,            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    A(KC_F8),     G(KC_T),      LSG(KC_O),     LSG(KC_P),    LSG(KC_9),        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    G(KC_I),      LSG(KC_I),    C(G(KC_O)),    LAG(KC_B),    KC_NO,            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    G(KC_DOT),    KC_NO,        LSG(KC_SPC),    G(KC_P),      KC_NO,            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    A(KC_F8),     G(KC_T),      LSG(KC_O),      LSG(KC_P),    LSG(KC_9),        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    G(KC_I),      LSG(KC_I),    C(G(KC_O)),     LAG(KC_B),    KC_NO,            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [8] = LAYOUT(
-    QK_BOOT,        HK_DUMP,       HK_SAVE,       HK_RESET,        HK_C_SCROLL,            KC_NO,    KC_NO,      KC_NO,    KC_NO,    QK_BOOT,
-    QK_C_EEPROM,    HK_P_SET_D,    HK_P_SET_S,    HK_P_SET_BUF,    HK_S_MODE_T,            KC_NO,    KC_DOWN,    KC_UP,    KC_NO,    QK_C_EEPROM,
+    QK_BOOT,        HK_DUMP,       HK_SAVE,       HK_RESET,        HK_C_SCROLL,            KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,
+    QK_C_EEPROM,    HK_P_SET_D,    HK_P_SET_S,    HK_P_SET_BUF,    HK_S_MODE_T,            KC_NO,    KC_DOWN,    KC_UP,    KC_NO,    KC_NO,
     KC_LSFT,        KC_NO,         KC_NO,         KC_NO,           HK_D_MODE_T,            KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,
                                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
